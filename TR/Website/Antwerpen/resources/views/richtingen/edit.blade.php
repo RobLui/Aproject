97f146ca7649e2043bd069cccd43dbd10f9a209b
Robbert Luit <robbertluit@hotmail.com>
2017-03-14 16:53:04 +0100

    back2info nl

--- TR/Website/Antwerpen/resources/views/richtingen/edit.blade.php
+++ TR/Website/Antwerpen/resources/views/richtingen/edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
         <div class="col-md-5">
           <!--DELETE BUTTON  -->
-          <a href="/../info">← back to overview</a>
+          <a href="/../info">← Terug naar info</a>
             <br><br>
               <div class="panel panel-default">
                 <div class="panel-heading">Edit richting
