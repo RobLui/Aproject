148b3a1e8d23bbfe4e4dd1563a7f74ba25207acd
Robbert Luit <robbertluit@hotmail.com>
2017-03-14 19:11:50 +0100

    ThessaRock! @news

--- TR/Website/Antwerpen/resources/views/links/nieuws.blade.php
+++ TR/Website/Antwerpen/resources/views/links/nieuws.blade.php
@@ -4,43 +4,30 @@
 <title>Nieuws</title>
 <div class="container">
   <div class="row">
-    <div class="col-md-10 col-md-offset-1">
       @include("common.messages")
       <div class="panel panel-default">
         <div class="panel-heading">Nieuws</div>
         <div class="panel-content">
           <form action="/event/add" method="post">
-            <button type="submit" name="button" class="x">test toevoegen</button>
+            <button type="submit" name="button" class="x">test toevoegen</button><br>
             {{ csrf_field() }}
           </form>
-          <ul class="article-overview">
             @if(count($events) > 0)
               @foreach($events as $event)
-                 <li>
-                   <tr>
-                    <th>
-                      <div class="testClass">
-                       <div class="url">&nbsp;
-                         <a href="#" class="urlTitle">{{$event->title}}</a>
-                           <p>
-                             {{$event->text}}
-                          </p>
-                         @if(isset(Auth::user()->name))
-                            @if(Auth::user()->name == $event->posted_by)
-                              <a href="article/edit/{{$event->id}}" class="btn btn-primary btn-xs edit-btn">edit</a>
-                            @endif
-                          @endif
-                      </div>
-                    </div>
-                  </th>
-                 </tr>
-                </li>
+              <div class="rowX">
+                  <h3>{{$event->title}}</h3>
+                  <p>{{$event->text}}</p>
+                  @if(isset(Auth::user()->name))
+                    @if(Auth::user()->name == $event->posted_by)
+                      <a href="article/edit/{{$event->id}}" class="btn btn-primary btn-xs edit-btn">edit</a>
+                    @endif
+                  @endif
+              </div>
               @endforeach
             @endif
-          </ul>
         </div>
-      </div>
-    </div>
   </div>
 </div>
+
+
 @endsection
