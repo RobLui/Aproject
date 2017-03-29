7545c55fbbf0d407d3feebae6305c818190c8379
Robbert Luit <robbertluit@hotmail.com>
2017-03-29 17:55:33 +0200

    add finished homepage

--- TR/Website/Antwerpen/resources/views/index.blade.php
+++ TR/Website/Antwerpen/resources/views/index.blade.php
@@ -2,5 +2,38 @@
 
 @section('content')
 <title>Home</title>
+<div class="container index">
+  <div class="slider">
 
+  </div>
+  <div class="col-sm-4 item"> <!-- 1 -->
+      <i class="fa fa-laptop"></i>
+      <h4>Ontdek de stad</h4>
+      <img src="images/contents/300/1.jpg" alt="placeholder">
+      <p>Vestibulum porta tristique porttitor donec at velit ac nunc blandit mollis donec ornare quis lorem eu iaculis curabitur scelerisque tincidunt lorem quis rhoncus.</p>
+      <div class="btn-section">
+          <a href="page_services.html" class="btn-custom"> + Lees meer </a>
+      </div>
+  </div>
+
+  <div class="col-sm-4 item"> <!-- 2 -->
+      <i class="fa fa-file-text-o"></i>
+      <h4>Beschikbare Richtingen</h4>
+      <img src="images/contents/300/1.jpg" alt="placeholder">
+      <p>Vestibulum porta tristique porttitor donec at velit ac nunc blandit mollis donec ornare quis lorem eu iaculis curabitur scelerisque tincidunt lorem quis rhoncus.</p>
+      <div class="btn-section">
+          <a href="page_services.html" class="btn-custom"> + Lees meer </a>
+      </div>
+  </div>
+
+  <div class="col-sm-4 item"> <!-- 3 -->
+      <i class="fa fa-globe"></i>
+      <h4>Nieuws en Evenementen</h4>
+      <img src="images/contents/300/1.jpg" alt="placeholder">
+      <p>Vestibulum porta tristique porttitor donec at velit ac nunc blandit mollis donec ornare quis lorem eu iaculis curabitur scelerisque tincidunt lorem quis rhoncus.</p>
+      <div class="btn-section">
+          <a href="page_services.html" class="btn-custom"> + Lees meer </a>
+      </div>
+  </div>
+</div>
 @endsection
