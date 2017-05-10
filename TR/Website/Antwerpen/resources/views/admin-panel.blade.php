d52afaa1accd393935aa40a96c31cf8123cc4147
Robbert Luit <robbertluit@hotmail.com>
2017-05-10 14:35:59 +0200

    add changes for mobile first dev

--- TR/Website/Antwerpen/resources/views/admin-panel.blade.php
+++ TR/Website/Antwerpen/resources/views/admin-panel.blade.php
@@ -2,4 +2,19 @@
 
 @section('content')
 
+<title>Admin-panel</title>
+<div class="container">
+
+  <div class="panel panel-default">
+    <h2>Secties</h2>
+  </div>
+  <div class="mid_container">
+    <div class="left-sq">
+      <a href="#">Nieuws &#38 Evenementen</a>
+    </div>
+    <div class="right-sq">
+      <a href="#">Opleidingsaanbod</a>
+    </div>
+  </div>
+</div>
 @endsection
