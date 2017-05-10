56abc9dbc27021742ac0435fa98cd4c7edcda165
Thessa Moerloos <thessa.moerloos@student.kdg.be>
2017-05-10 22:26:27 +0200

    ontdek en index responsive

--- TR/Website/Antwerpen/resources/views/links/ontdek.blade.php
+++ TR/Website/Antwerpen/resources/views/links/ontdek.blade.php
@@ -17,10 +17,10 @@
 			    <h2>Ontdek de stad</h2>
 			  </div>
 
-			<div class="col-sm-4 item">
+			<div class="col-sm-4 ontdek item">
 				<!--  PLACES TO EAT -->
 				<li class="fa fa-cutlery fa-3x "></li>
-				<h1>Places to eat</h1>
+				<h1>Eet plekskes</h1>
 				<div class="blok">
 					<img src="#" alt="300">
 					<p>Vestibulum porta tristique porttitor donec at velit ac nunc blandit mollis donec ornare quis lorem eu iaculis curabitur scelerisque tincidunt lorem quis rhoncus.</p>
@@ -30,10 +30,10 @@
 				</div>
 			</div>
 			<!--  PLACES TO STUDY -->
-			<div class="col-sm-4 item">
+			<div class="col-sm-4 ontdek item">
 				<!-- 2 -->
 				<li class="fa fa-book fa-3x"></li>
-				<h1>Places to study</h1>
+				<h1>Studie plekskes</h1>
 				<div class="blok">
 					<img src="images/contents/300/1.jpg" alt="300">
 					<p class="columnText">Vestibulum porta tristique porttitor donec at velit ac nunc blandit mollis donec ornare quis lorem eu iaculis curabitur scelerisque tincidunt lorem quis rhoncus.</p>
@@ -44,10 +44,10 @@
 			</div>
 
 			<!--  PLACES TO SEE -->
-			<div class="col-sm-4 item">
+			<div class="col-sm-4 ontdek item">
 				<!-- 3 -->
 				<li class="fa fa-search fa-3x" aria-hidden="true"></li>
-				<h1>Places to see</h1>
+				<h1>Schoon plekskes</h1>
 				<div class="blok">
 					<img src="images/contents/300/1.jpg" alt="300">
 					<p class="columnText">Vestibulum porta tristique porttitor donec at velit ac nunc blandit mollis donec ornare quis lorem eu iaculis curabitur scelerisque tincidunt lorem quis rhoncus.</p>
