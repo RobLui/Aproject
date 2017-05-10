cb31acde3b378331b950f1f13d201b9b5c68421c
Robbert Luit <robbertluit@hotmail.com>
2017-05-10 15:37:51 +0200

    add latest resp changes to admin-panel & nieuws

--- TR/Website/Antwerpen/resources/views/admin-panel.blade.php
+++ TR/Website/Antwerpen/resources/views/admin-panel.blade.php
@@ -4,16 +4,17 @@
 
 <title>Admin-panel</title>
 <div class="container">
-
   <div class="panel panel-default">
-    <h2>Secties</h2>
+    <h2>Admin</h2>
   </div>
-  <div class="mid_container">
-    <div class="left-sq">
-      <a href="#">Nieuws &#38 Evenementen</a>
-    </div>
-    <div class="right-sq">
-      <a href="#">Opleidingsaanbod</a>
+  <div class="panel-content">
+    <div class="mid_container">
+      <div class="left-sq">
+        <a href="/nieuws">Nieuws &amp Evenementen</a>
+      </div>
+      <div class="right-sq">
+        <a href="/info/studieaanbod">Opleidingsaanbod</a>
+      </div>
     </div>
   </div>
 </div>
--- TR/Website/Antwerpen/resources/views/nieuws/nieuws.blade.php
+++ TR/Website/Antwerpen/resources/views/nieuws/nieuws.blade.php
@@ -2,36 +2,38 @@
 
 @section('content')
 <title>Nieuws</title>
-  <div class="container">
-    <div class="row">
-      @include("common.messages")
-      <div class="panel panel-default">
-          <h2>Nieuws &amp Events</h2>
-        </div>
-      </div>
-        <div class="panel-content">
-          @if(isset(Auth::user()->name))
-            @if(Auth::user()->name == "Admin")
-              <form action="/nieuws/add" method="post">
-                <button type="submit" name="button" class="x">test toevoegen</button><br>
-                {{ csrf_field() }}
-              </form>
-            @endif
-          @endif
-            @if(count($events) > 0)
-              @foreach($events as $event)
-              <div class="row_nieuws">
-                  <h3>{{$event->title}}</h3>
-                  <p>{{$event->text}}</p>
-                  @if(isset(Auth::user()->name))
-                    @if(Auth::user()->name == $event->posted_by)
-                      <a href="nieuws/edit/{{$event->id}}" class="btn btn-primary btn-xs edit-btn">edit</a>
-                    @endif
-                  @endif
-                </div>
-              @endforeach
-            @endif
-        </div>
+
+<div class="container">
+  <div class="row">
+    @include("common.messages")
+    <div class="panel panel-default">
+      <h2>Nieuws &amp Events</h2>
     </div>
   </div>
+
+  <div class="panel-content">
+    @if(isset(Auth::user()->name))
+      @if(Auth::user()->name == "Admin")
+        <form action="/nieuws/add" method="post">
+          <button type="submit" name="button" class="x">test toevoegen</button><br>
+          {{ csrf_field() }}
+        </form>
+      @endif
+    @endif
+    @if(count($events) > 0)
+      @foreach($events as $event)
+      <div class="row_nieuws">
+          <h3>{{$event->title}}</h3>
+          <p>{{$event->text}}</p>
+          @if(isset(Auth::user()->name))
+            @if(Auth::user()->name == $event->posted_by)
+              <a href="nieuws/edit/{{$event->id}}" class="btn btn-primary btn-xs edit-btn">edit</a>
+            @endif
+          @endif
+        </div>
+      @endforeach
+    @endif
+  </div>
+</div>
+
 @endsection
