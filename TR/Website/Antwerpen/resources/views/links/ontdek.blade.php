8bf32f4ea831798f13082b90b0a9e662abd2f9ae
Robbert Luit <robbertluit@hotmail.com>
2017-05-11 12:19:03 +0200

    add corrected up to date css styling

--- TR/Website/Antwerpen/resources/views/links/ontdek.blade.php
+++ TR/Website/Antwerpen/resources/views/links/ontdek.blade.php
@@ -17,12 +17,12 @@
 			    <h2>Ontdek de stad</h2>
 			  </div>
 
-			<div class="col-sm-4 ontdek item">
+			<div class="col-sm-4 item">
 				<!--  PLACES TO EAT -->
 				<li class="fa fa-cutlery fa-3x "></li>
 				<h1>Eet plekskes</h1>
-				<div class="blok">
-					<img src="#" alt="300">
+				<div class="block_img">
+					<img src="images/threehundred.jpg" alt="300">
 					<p>Vestibulum porta tristique porttitor donec at velit ac nunc blandit mollis donec ornare quis lorem eu iaculis curabitur scelerisque tincidunt lorem quis rhoncus.</p>
 					<div class="btn-section">
 						<a href="ontdek/places-to-eat" class="btn-custom"> > meer lezen </a>
@@ -30,12 +30,12 @@
 				</div>
 			</div>
 			<!--  PLACES TO STUDY -->
-			<div class="col-sm-4 ontdek item">
+			<div class="col-sm-4  item">
 				<!-- 2 -->
 				<li class="fa fa-book fa-3x"></li>
 				<h1>Studie plekskes</h1>
-				<div class="blok">
-					<img src="images/contents/1.jpg" alt="300">
+				<div class="block_img">
+					<img src="images/threehundred.jpg" alt="300">
 					<p class="columnText">Vestibulum porta tristique porttitor donec at velit ac nunc blandit mollis donec ornare quis lorem eu iaculis curabitur scelerisque tincidunt lorem quis rhoncus.</p>
 					<div class="btn-section">
 						<a href="ontdek/places-to-study" class="btn-custom"> >  meer lezen </a>
@@ -44,12 +44,12 @@
 			</div>
 
 			<!--  PLACES TO SEE -->
-			<div class="col-sm-4 ontdek item">
+			<div class="col-sm-4 item">
 				<!-- 3 -->
 				<li class="fa fa-search fa-3x" aria-hidden="true"></li>
 				<h1>Schoon plekskes</h1>
-				<div class="blok">
-					<img src="images/contents/300/1.jpg" alt="300">
+				<div class="block_img">
+					<img src="images/threehundred.jpg" alt="300">
 					<p class="columnText">Vestibulum porta tristique porttitor donec at velit ac nunc blandit mollis donec ornare quis lorem eu iaculis curabitur scelerisque tincidunt lorem quis rhoncus.</p>
 					<div class="btn-section">
 						<a href="ontdek/places-to-see" class="btn-custom"> > meer lezen </a>
