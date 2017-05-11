bff77a9ab660372439343dadd1a32200af7948a0
Thessa Moerloos <thessa.moerloos@student.kdg.be>
2017-05-11 11:11:10 +0200

    updaten tekst index en icons

--- TR/Website/Antwerpen/resources/views/index.blade.php
+++ TR/Website/Antwerpen/resources/views/index.blade.php
@@ -9,11 +9,14 @@
 
 <div class="homeContainer">
     <div class="col-sm-4 item"> <!-- 1 -->
-      <i class="fa fa-laptop fa-3x"></i>
+      <i class="fa fa-binoculars fa-3x"></i>
       <h1>Ontdek de stad</h1>
       <div class="block_img">
         <img src="images/threehundred.jpg" alt="img">
-        <p>Vestibulum porta tristique porttitor donec at velit ac nunc blandit mollis donec ornare quis lorem eu iaculis curabitur scelerisque tincidunt lorem quis rhoncus.</p>
+        <p>Nieuw in Antwerpen?
+          Wat valt er zoal te doen in ‘t Stad?
+          Wij geven je alvast 10 dingen die je zeker niet mag missen!
+        </p>
         <div class="btn-section">
           <a href="page_services.html" class="btn-custom"> > meer </a>
         </div>
@@ -21,11 +24,13 @@
     </div>
 
     <div class="col-sm-4 item"> <!-- 2 -->
-      <i class="fa fa-file-text-o fa-3x"></i>
+      <i class="fa fa-info fa-3x"></i>
         <h1>Beschikbare Richtingen</h1>
         <div class="block_img">
           <img src="images/threehundred.jpg" alt="img">
-          <p>Vestibulum porta tristique porttitor donec at velit ac nunc blandit mollis donec ornare quis lorem eu iaculis curabitur scelerisque tincidunt lorem quis rhoncus.</p>
+          <p>Ben je er nog niet helemaal uit wat je wil gaan studeren?
+            Vragen over je richting?
+            Geen nood! Hier is een overzicht te vinden van alle scholen en hun aanbod.</p>
           <div class="btn-section">
               <a href="/studieaanbod" class="btn-custom"> > meer </a>
           </div>
@@ -33,11 +38,15 @@
     </div>
 
     <div class="col-sm-4 item"> <!-- 3 -->
-      <i class="fa fa-calendar-o fa-3x" aria-hidden="true"></i>
+      <i class="fa fa-paper-plane fa-3x" aria-hidden="true"></i>
       <h1>Nieuws en Evenementen</h1>
       <div class="block_img">
         <img src="images/threehundred.jpg" alt="img">
-        <p>Vestibulum porta tristique porttitor donec at velit ac nunc blandit mollis donec ornare quis lorem eu iaculis curabitur scelerisque tincidunt lorem quis rhoncus.</p>
+        <p>Pauzeren hoort nu eenmaal bij studeren.
+          Waar gaan feestjes door? Al naar de Stu Day geweest?
+          Geïntereseerd in een nieuwe studierichtig?
+          Neem zeker en vast een kijkje!
+        </p>
         <div class="btn-section">
             <a href="page_services.html" class="btn-custom"> > meer </a>
         </div>
