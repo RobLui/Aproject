21be6bea9e36d28af642ad862509ba22d1a35d30
Thessa Moerloos <thessa.moerloos@student.kdg.be>
2017-05-15 21:42:10 +0200

    footer afwerken + icons

--- TR/Website/Antwerpen/resources/views/includes/footer.blade.php
+++ TR/Website/Antwerpen/resources/views/includes/footer.blade.php
@@ -1,7 +1,31 @@
 <!-- #footer -->
 <section id="footer">
 
-    <img src="/images/footerImg.jpg" alt="footerImg" class="footerImg">
+  <div class="footerContentContainer">
+
+    <div class="footerLMR">
+
+        <div class="footerItem">
+          <a href="www.antwerpen.be">www.antwerpen.be</a>
+        </div>
+
+        <div class="footerItem">
+          <a href="www.antwerpen.be"><img src="/images/fb.png" alt="facebook"  class="footerLogo"></a>
+          <a href="www.antwerpen.be"><img src="/images/googlePlay.png" alt="googlePlay" class="footerLogo"></a>
+          <a href="www.antwerpen.be"><img src="/images/appleStore.png" alt="appleStore" class="footerLogo"></a>
+        </div>
+
+        <div class="footerItem">
+          <a href="www.gate15.be">www.gate15.be</a>
+        </div>
+
+    </div>
+
+    <img src="/images/footer2.jpg" alt="footerImg" class="footerImg">
+
+  </div>
+
+    <!-- <img src="/images/footer2.jpg" alt="footerImg" class="footerImg"> -->
 
     <!-- .container -->
     <div class="footerContainer">
